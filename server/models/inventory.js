a758e82717b9103b8aad664d5e52a6cf98737f3e
alvyynm <alvyynm@gmail.com>
2023-08-09 07:22:05 +0300

    create inventory model

---
+++ server/models/inventory.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const dataSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  unitPrice: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  quantityInStock: {
+    type: Number,
+    required: true,
+  },
+  shelfNumber: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("dataSchema", dataSchema);
