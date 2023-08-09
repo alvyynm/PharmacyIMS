fdf6a48ec9a48cf70daf798f7903a3c07c6565ae
alvyynm <alvyynm@gmail.com>
2023-08-09 07:15:07 +0300

    check for user login status

---
+++ server/middleware/is-auth.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  const authHeader = req.get("Authorization");
+
+  // check if Authorization header is included in request top determine is user is authenticated
+  if (!authHeader) {
+    const error = new Error("Not auntheticated");
+    error.statusCode = 401;
+    throw error;
+  }
+
+  const token = authHeader.split(" ")[1];
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, "secret");
+  } catch (error) {
+    err.statusCode = 500;
+    throw error;
+  }
+
+  if (!decodedToken) {
+    const error = new Error("Not auntheticated");
+    error.statusCode = 401;
+    throw error;
+  }
+
+  req.userId = decodedToken.userId;
+  next();
+};
