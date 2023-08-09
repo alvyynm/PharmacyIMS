565455bb451b0acbc5c5fcfe1a6e2db76d407d90
alvyynm <alvyynm@gmail.com>
2023-08-09 09:50:45 +0300

    create product routes with controllers

---
+++ server/routes/data.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const data = require("../models/inventory");
+const isAuth = require("../middleware/is-auth");
+const router = express.Router();
+
+const dataController = require("../controllers/data");
+
+// GET /v1/products
+router.get("/products", isAuth, dataController.getProducts);
+
+// POST /v1/product
+router.post(
+  "/post",
+  isAuth,
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  dataController.createProduct
+);
+
+//GET /v1/post/postId
+router.get("/post/:postId", isAuth, dataController.getPost);
+
+//PUT /v1/product/productId
+router.put(
+  "/product/:productId",
+  isAuth,
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  dataController.updateProduct
+);
+
+//DELETE /v1/product/productId
+router.delete("/product/:productId", isAuth, feedController.deleteProduct);
+
+module.exports = router;
