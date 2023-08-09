1e46b3a02f6f02bfa3d88c5ce318e6362bd52dec
alvyynm <alvyynm@gmail.com>
2023-08-09 09:52:20 +0300

    create auth routes with controllers

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -5,103 +5,103 @@ require("dotenv").config();
 
 const secret = process.env.MY_SECRET_KEY;
 
-// const User = require("../models/user");
+const User = require("../models/user");
 
-// exports.signup = (req, res, next) => {
-//   const errors = validationResult(req);
+exports.signup = (req, res, next) => {
+  const errors = validationResult(req);
 
-//   if (!errors.isEmpty()) {
-//     const error = new Error("Invalid credentials");
-//     error.statusCode = 422;
-//     error.data = errors.array();
-//     throw error;
-//   }
+  if (!errors.isEmpty()) {
+    const error = new Error("Invalid credentials");
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
 
-//   const email = req.body.email;
-//   const password = req.body.password;
-//   const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+  const name = req.body.name;
+  const role = req.body.role;
 
-//   bcrypt
-//     .hash(password, 12)
-//     .then((hashedPwd) => {
-//       const user = new User({
-//         email: email,
-//         password: hashedPwd,
-//         name: name,
-//       });
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPwd) => {
+      const user = new User({
+        email: email,
+        password: hashedPwd,
+        name: name,
+        role: role,
+      });
 
-//       return user.save();
-//     })
-//     .then((result) => {
-//       res.status(201).json({
-//         message: "User account created successfully!",
-//         userId: result._id,
-//       });
-//     })
-//     .catch((error) => {
-//       if (!error.statusCode) {
-//         error.statusCode = 500;
-//       }
-//       next(error);
-//     });
-// };
+      return user.save();
+    })
+    .then((result) => {
+      res.status(201).json({
+        message: "User account created successfully!",
+        userId: result._id,
+      });
+    })
+    .catch((error) => {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next(error);
+    });
+};
 
-// exports.login = (req, res, next) => {
-//   const errors = validationResult(req);
+exports.login = (req, res, next) => {
+  const errors = validationResult(req);
 
-//   if (!errors.isEmpty()) {
-//     const error = new Error("Invalid credentials");
-//     error.statusCode = 422;
-//     error.data = errors.array();
-//     throw error;
-//   }
+  if (!errors.isEmpty()) {
+    const error = new Error("Invalid credentials");
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
 
-//   const email = req.body.email;
-//   const password = req.body.password;
-//   let loggedInUser;
+  const email = req.body.email;
+  const password = req.body.password;
+  let loggedInUser;
 
-//   User.findOne({ email: email })
-//     .then((user) => {
-//       if (!user) {
-//         const error = new Error("Invalid credentials");
-//         error.statusCode = 401;
-//         throw error;
-//       }
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        const error = new Error("Invalid credentials");
+        error.statusCode = 401;
+        throw error;
+      }
 
-//       loggedInUser = user;
-//       return bcrypt.compare(password, user.password);
-//     })
-//     .then((isEqual) => {
-//       if (!isEqual) {
-//         const error = new Error("Invalid credentials");
-//         error.statusCode = 401;
-//         throw error;
-//       }
+      loggedInUser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then((isEqual) => {
+      if (!isEqual) {
+        const error = new Error("Invalid credentials");
+        error.statusCode = 401;
+        throw error;
+      }
 
-//       // create jwt token and return it to the user
-//       const token = jwt.sign(
-//         {
-//           email: loggedInUser.email,
-//           userId: loggedInUser._id.toString(),
-//         },
-//         secret,
-//         {
-//           expiresIn: "1h",
-//         }
-//       );
-//       res.status(200).json({
-//         message: "Login successful!",
-//         token: token,
-//         userId: loggedInUser._id.toString(),
-//       });
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       if (!error.statusCode) {
-//         error.statusCode = 500;
-//       }
-//       next(error);
-//     });
-// };
-
-console.log("User account created successfully!");
+      // create jwt token and return it to the user
+      const token = jwt.sign(
+        {
+          email: loggedInUser.email,
+          userId: loggedInUser._id.toString(),
+        },
+        secret,
+        {
+          expiresIn: "1h",
+        }
+      );
+      res.status(200).json({
+        message: "Login successful!",
+        token: token,
+        userId: loggedInUser._id.toString(),
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next(error);
+    });
+};
