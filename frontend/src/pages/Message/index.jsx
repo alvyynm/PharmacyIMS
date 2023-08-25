7f141683e9e55ac1a6920268eed8e1fd95899141
alvyynm <alvyynm@gmail.com>
2023-08-25 05:18:09 +0300

    add edit and delete functionality

--- frontend/src/pages/Message/index.jsx
+++ frontend/src/pages/Message/index.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import { data } from '../../data/Data';
-import { Table, Modal } from 'antd';
-import { DeleteOutlined } from '@ant-design/icons';
+import { Table, Modal, Input } from 'antd';
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
 import Navbar from '../../components/Navbar';
 import Topbar from '../../components/Topbar';
 
 function index() {
   const [Data, setData] = useState(data);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [edit, setEdit] = useState(null);
+
   const columns = [
     {
       key: 'name',
@@ -39,7 +42,10 @@ function index() {
       render: (record) => {
         return (
           <>
-            <DeleteOutlined style={{ color: 'red' }} onClick={() => Delete(record)} />
+            <div className="flex gap-4">
+              <EditOutlined style={{ color: 'black' }} onClick={() => Edit(record)} />
+              <DeleteOutlined style={{ color: 'red' }} onClick={() => Delete(record)} />
+            </div>
           </>
         );
       },
@@ -48,7 +54,10 @@ function index() {
 
   const Delete = (record) => {
     Modal.confirm({
-      title: 'Are you sure you want to delete this',
+      title: 'Are you sure you want to delete this data',
+      okText: 'Yes',
+      okType: 'danger',
+      cancelText: 'No',
       onOk: () => {
         setData((pre) => {
           return pre.filter((person) => person.id != record.id);
@@ -56,6 +65,19 @@ function index() {
       },
     });
   };
+
+  // Edit records
+  const Edit = (record) => {
+    setIsModalOpen(true);
+    setEdit({ ...record });
+  };
+
+  // Cancel editing and reset data to initial state
+  const ResetEditing = () => {
+    setEdit(null);
+    setIsModalOpen(false);
+  };
+
   return (
     <section className="relative">
       <div className="grid grid-cols-6 grid-rows-1 gap-12">
@@ -70,6 +92,70 @@ function index() {
           </div>
           <h1>Latest Data</h1>
           <Table dataSource={Data} columns={columns} pagination={false} />
+          <Modal
+            title="Edit Details"
+            open={isModalOpen}
+            okText="Save"
+            onCancel={() => ResetEditing()}
+            onOk={() => {
+              setData((pre) => {
+                return pre.map((student) => {
+                  if (student.id === edit.id) {
+                    return edit;
+                  } else {
+                    return student;
+                  }
+                });
+              });
+              ResetEditing();
+            }}
+          >
+            <Input
+              value={edit?.name}
+              onChange={(e) => {
+                setEdit((pre) => {
+                  return { ...pre, name: e.target.value };
+                });
+              }}
+              className="mb-3 rounded-lg"
+            />
+            <Input
+              value={edit?.email}
+              onChange={(e) => {
+                setEdit((pre) => {
+                  return { ...pre, email: e.target.value };
+                });
+              }}
+              className="mb-3 rounded-lg"
+            />
+            <Input
+              value={edit?.address}
+              onChange={(e) => {
+                setEdit((pre) => {
+                  return { ...pre, address: e.target.value };
+                });
+              }}
+              className="mb-3 rounded-lg"
+            />
+            <Input
+              value={edit?.phone}
+              onChange={(e) => {
+                setEdit((pre) => {
+                  return { ...pre, phone: e.target.value };
+                });
+              }}
+              className="mb-3 rounded-lg"
+            />
+            <Input
+              value={edit?.website}
+              onChange={(e) => {
+                setEdit((pre) => {
+                  return { ...pre, website: e.target.value };
+                });
+              }}
+              className="mb-3 rounded-lg"
+            />
+          </Modal>
         </main>
       </div>
     </section>
