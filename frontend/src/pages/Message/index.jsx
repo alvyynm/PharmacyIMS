231b941231c3654550eb6c05233e4f24563bbc2e
alvyynm <alvyynm@gmail.com>
2023-08-25 08:10:31 +0300

    add create record functionality

--- frontend/src/pages/Message/index.jsx
+++ frontend/src/pages/Message/index.jsx
@@ -8,7 +8,9 @@ import Topbar from '../../components/Topbar';
 function index() {
   const [Data, setData] = useState(data);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
   const [edit, setEdit] = useState(null);
+  const [formData, setFormData] = useState(null);
 
   const columns = [
     {
@@ -78,6 +80,30 @@ function index() {
     setIsModalOpen(false);
   };
 
+  // Create new record
+
+  const onAddRecord = (record) => {
+    const randomNumber = parseInt(Math.random() * 1000);
+    const newRecord = {
+      id: randomNumber,
+      name: `name`,
+      email: `email`,
+      address: `address`,
+      phone: 123 - 456 - 789,
+      website: `website`,
+    };
+    console.log(newRecord);
+    setData((pre) => {
+      return [...pre, newRecord];
+    });
+  };
+
+  // Discard new record
+  const onCancelAdd = () => {
+    setIsAddModalOpen(false);
+    setFormData(null);
+  };
+
   return (
     <section className="relative">
       <div className="grid grid-cols-6 grid-rows-1 gap-12">
@@ -90,8 +116,22 @@ function index() {
           <div className="sticky top-0 left-0 right-0 z-50 bg-white">
             <Topbar />
           </div>
-          <h1>Latest Data</h1>
+          <h1 className="my-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            List of drugs
+          </h1>
+          <div>
+            <button
+              onClick={() => {
+                setIsAddModalOpen(true);
+              }}
+              className="flex w-48 justify-center rounded-md mb-4 bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Add new record
+            </button>
+          </div>
           <Table dataSource={Data} columns={columns} pagination={false} />
+
+          {/* Edit record modal */}
           <Modal
             title="Edit Details"
             open={isModalOpen}
@@ -110,7 +150,9 @@ function index() {
               ResetEditing();
             }}
           >
+            <label htmlFor="name">Name</label>
             <Input
+              id="name"
               value={edit?.name}
               onChange={(e) => {
                 setEdit((pre) => {
@@ -119,7 +161,9 @@ function index() {
               }}
               className="mb-3 rounded-lg"
             />
+            <label htmlFor="email">Email</label>
             <Input
+              id="email"
               value={edit?.email}
               onChange={(e) => {
                 setEdit((pre) => {
@@ -128,7 +172,9 @@ function index() {
               }}
               className="mb-3 rounded-lg"
             />
+            <label htmlFor="address">Address</label>
             <Input
+              id="address"
               value={edit?.address}
               onChange={(e) => {
                 setEdit((pre) => {
@@ -137,7 +183,9 @@ function index() {
               }}
               className="mb-3 rounded-lg"
             />
+            <label htmlFor="phone">Phone</label>
             <Input
+              id="phone"
               value={edit?.phone}
               onChange={(e) => {
                 setEdit((pre) => {
@@ -146,7 +194,9 @@ function index() {
               }}
               className="mb-3 rounded-lg"
             />
+            <label htmlFor="website">Website</label>
             <Input
+              id="website"
               value={edit?.website}
               onChange={(e) => {
                 setEdit((pre) => {
@@ -156,6 +206,84 @@ function index() {
               className="mb-3 rounded-lg"
             />
           </Modal>
+
+          {/* Add record modal */}
+          <Modal
+            title="Add new record"
+            open={isAddModalOpen}
+            okText="Save record"
+            onCancel={() => onCancelAdd()}
+            onOk={() => {
+              onAddRecord();
+              onCancelAdd();
+            }}
+          >
+            <label htmlFor="name">Name</label>
+            <Input
+              id="name"
+              value={edit?.name}
+              onChange={(e) => {
+                setEdit((pre) => {
+                  return { ...pre, name: e.target.value };
+                });
+              }}
+              className="mb-3 rounded-lg"
+            />
+            <label htmlFor="email">Email</label>
+            <Input
+              id="email"
+              value={edit?.email}
+              onChange={(e) => {
+                setEdit((pre) => {
+                  return { ...pre, email: e.target.value };
+                });
+              }}
+              className="mb-3 rounded-lg"
+            />
+            <label htmlFor="address">Address</label>
+            <Input
+              id="address"
+              value={edit?.address}
+              onChange={(e) => {
+                setEdit((pre) => {
+                  return { ...pre, address: e.target.value };
+                });
+              }}
+              className="mb-3 rounded-lg"
+            />
+            <label htmlFor="phone">Phone</label>
+            <Input
+              id="phone"
+              value={edit?.phone}
+              onChange={(e) => {
+                setEdit((pre) => {
+                  return { ...pre, phone: e.target.value };
+                });
+              }}
+              className="mb-3 rounded-lg"
+            />
+            <label htmlFor="website">Website</label>
+            <Input
+              id="website"
+              value={edit?.website}
+              onChange={(e) => {
+                setEdit((pre) => {
+                  return { ...pre, website: e.target.value };
+                });
+              }}
+              className="mb-3 rounded-lg"
+            />
+          </Modal>
+          <div>
+            <button
+              onClick={() => {
+                setIsAddModalOpen(true);
+              }}
+              className="flex w-48 justify-center rounded-md mb-4 bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Add new record
+            </button>
+          </div>
         </main>
       </div>
     </section>
