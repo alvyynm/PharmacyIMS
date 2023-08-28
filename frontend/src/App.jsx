e3a4c513989efafcddf035bac2b45a5a0a9e2ce7
alvyynm <alvyynm@gmail.com>
2023-08-28 11:42:12 +0300

    consume AuthContext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthContext } from './context/AuthContext';
 
 import './App.css';
 import '@tremor/react/dist/esm/tremor.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import Dashboard from './pages/Dashboard';
 import Message from './pages/Message';
@@ -12,25 +16,31 @@ import Reports from './pages/Reports';
 import Admin from './pages/Admin';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(undefined);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/login"
-          element={
-            <div className="font-primary">
-              <Login />
-            </div>
-          }
-        ></Route>
-        <Route path="/dashboard" element={<Dashboard />}></Route>
-        <Route path="/message" element={<Message />}></Route>
-        <Route path="/settings" element={<Settings />}></Route>
-        <Route path="/signup" element={<Signup />}></Route>
-        <Route path="/reports" element={<Reports />}></Route>
-        <Route path="/admin" element={<Admin />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn, user, setUser }}>
+      <BrowserRouter>
+        <ToastContainer />
+        <Routes>
+          <Route
+            path="/login"
+            element={
+              <div className="font-primary">
+                <Login />
+              </div>
+            }
+          ></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/message" element={<Message />}></Route>
+          <Route path="/settings" element={<Settings />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/reports" element={<Reports />}></Route>
+          <Route path="/admin" element={<Admin />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
