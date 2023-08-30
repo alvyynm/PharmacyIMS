ee7a611d113ab3e88aa18483f8f1459fe3f48a88
alvyynm <alvyynm@gmail.com>
2023-08-30 07:52:21 +0300

    FE: create DataContext for storing database data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthContext } from './context/AuthContext';
 import { UserContext } from './context/UserContext';
+import { DataContext } from './context/DataContext';
 
 import './App.css';
 import '@tremor/react/dist/esm/tremor.css';
@@ -19,30 +20,33 @@ import Admin from './pages/Admin';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [user, setUser] = useState(null);
+  const [inventory, setInventory] = useState(null);
 
   return (
     <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
       <UserContext.Provider value={{ user, setUser }}>
-        <BrowserRouter>
-          <ToastContainer />
-          <Routes>
-            <Route path="/" element={<Login />}></Route>
-            <Route
-              path="/login"
-              element={
-                <div className="font-primary">
-                  <Login />
-                </div>
-              }
-            ></Route>
-            <Route path="/dashboard" element={<Dashboard />}></Route>
-            <Route path="/message" element={<Message />}></Route>
-            <Route path="/settings" element={<Settings />}></Route>
-            <Route path="/signup" element={<Signup />}></Route>
-            <Route path="/reports" element={<Reports />}></Route>
-            <Route path="/admin" element={<Admin />}></Route>
-          </Routes>
-        </BrowserRouter>
+        <DataContext.Provider value={{ inventory, setInventory }}>
+          <BrowserRouter>
+            <ToastContainer />
+            <Routes>
+              <Route path="/" element={<Login />}></Route>
+              <Route
+                path="/login"
+                element={
+                  <div className="font-primary">
+                    <Login />
+                  </div>
+                }
+              ></Route>
+              <Route path="/dashboard" element={<Dashboard />}></Route>
+              <Route path="/message" element={<Message />}></Route>
+              <Route path="/settings" element={<Settings />}></Route>
+              <Route path="/signup" element={<Signup />}></Route>
+              <Route path="/reports" element={<Reports />}></Route>
+              <Route path="/admin" element={<Admin />}></Route>
+            </Routes>
+          </BrowserRouter>
+        </DataContext.Provider>
       </UserContext.Provider>
     </AuthContext.Provider>
   );
