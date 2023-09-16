e8c9f2f0ad2efb315c82d1e1ba44ce68a65b9b6f
alvyynm <alvyynm@gmail.com>
2023-09-16 13:35:17 +0300

    FE: add input validation on login form

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { toast } from 'react-toastify';
+import { useForm } from 'react-hook-form';
 import { AuthContext } from '../../context/AuthContext';
 import { UserContext } from '../../context/UserContext';
 import { authValidator } from '../../utils/authVerify';
@@ -14,23 +15,27 @@ export default function index() {
   const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
   const { user, setUser } = useContext(UserContext);
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
   const notify = () => {
     toast.error('Ooops! No credentials provided', {
       position: toast.POSITION.TOP_LEFT,
     });
   };
 
-  const handleLogin = async (e) => {
-    e.preventDefault(); // Prevent the default form submission behavior
-
-    if (email !== '' && password !== '') {
+  const handleLogin = async (data) => {
+    if (data.email !== '' && data.password !== '') {
       try {
         const response = await axios.post('http://localhost:3001/auth/login', {
-          email: email,
-          password: password,
+          email: data.email,
+          password: data.password,
         });
 
-        console.log(email, password);
+        console.log(data.email, data.password);
 
         console.log(response.data); // log api response to console
 
@@ -79,7 +84,7 @@ export default function index() {
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form onSubmit={handleLogin} className="space-y-6" action="#" method="POST">
+          <form onSubmit={handleSubmit(handleLogin)} className="space-y-6" action="#" method="POST">
             <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                 Email address
@@ -89,12 +94,22 @@ export default function index() {
                   id="email"
                   name="email"
                   type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
                   autoComplete="email"
-                  required
+                  {...register('email', {
+                    required: 'Email is required',
+                    validate: {
+                      maxLength: (v) =>
+                        v.length <= 50 || 'The email should have at most 50 characters',
+                      matchPattern: (v) =>
+                        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
+                        'Email address must be a valid address',
+                    },
+                  })}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
+                {errors.email?.message && (
+                  <small className="text-red-600">{errors.email.message}</small>
+                )}
               </div>
             </div>
 
@@ -117,12 +132,14 @@ export default function index() {
                   id="password"
                   name="password"
                   type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  autoComplete="current-password"
-                  required
+                  {...register('password', {
+                    required: true,
+                  })}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
+                {errors.password?.type === 'required' && (
+                  <small className="text-red-600">Password is required</small>
+                )}
               </div>
             </div>
 
