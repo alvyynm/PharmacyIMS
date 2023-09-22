26fd286870a34f075e9a5315c583f825928c5cad
alvyynm <alvyynm@gmail.com>
2023-09-22 06:54:37 +0300

    FE: display archive data

--- frontend/src/pages/Archive/index.jsx
+++ frontend/src/pages/Archive/index.jsx
@@ -1,28 +1,55 @@
-import React, { useState, useContext, useRef } from 'react';
+import React, { useState, useContext, useRef, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 import { Table, Button, DatePicker, Input } from 'antd';
 import moment from 'moment';
+import axios from 'axios';
 import Navbar from '../../components/Navbar';
 import Topbar from '../../components/Topbar';
 import { AuthContext } from '../../context/AuthContext';
-import { DataContext } from '../../context/DataContext';
+import { ArchiveContext } from '../../context/ArchiveContext';
 import { useReactToPrint } from 'react-to-print';
 const { RangePicker } = DatePicker;
 
 function index() {
   const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
-  const { inventory, setInventory } = useContext(DataContext);
+  const { archive, setArchive } = useContext(ArchiveContext);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
-  const [filteredData, setFilteredData] = useState(inventory);
+  const [filteredData, setFilteredData] = useState(archive);
 
-  const [filteredQData, setFilteredQData] = useState(inventory);
+  const [filteredQData, setFilteredQData] = useState(archive);
   const [filterValue, setFilterValue] = useState('');
 
+  // Retrieve the token from local storage
+  const token = localStorage.getItem('token');
+
+  useEffect(() => {
+    // API call to retrieve archive data from database
+    axios
+      .get('http://localhost:3001/v1/archiveProducts', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        setArchive(response.data.products);
+        console.log(response.data.products);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, [token]);
+
+  // ARCHIVE DATA FILTER
   const handleDateFilter = () => {
-    const filteredResult = inventory.filter((item) => {
-      const itemDate = moment(item.expiryDate);
+    const filteredResult = archive.filter((item) => {
+      const itemDate = moment(item.archiveDate);
       if (startDate && endDate) {
         console.log(startDate, endDate);
         return itemDate.isBetween(startDate, endDate, null, '[]');
@@ -35,13 +62,13 @@ function index() {
   const clearDateFilter = () => {
     setStartDate(null);
     setEndDate(null);
-    setFilteredData(inventory);
+    setFilteredData(archive);
   };
 
   // Quantity filter
   const handleFilter = () => {
     console.log('method running', filterValue);
-    const filteredResult = inventory.filter((record) => {
+    const filteredResult = archive.filter((record) => {
       if (record.quantityInStock < parseFloat(filterValue)) {
         return record;
       }
@@ -51,7 +78,7 @@ function index() {
 
   const clearFilter = () => {
     setFilterValue('');
-    setFilteredData(inventory);
+    setFilteredData(archive);
   };
 
   // END OF QUANTITY FILTER
@@ -65,22 +92,22 @@ function index() {
     {
       key: 'name',
       title: 'Name',
-      dataIndex: 'name',
+      dataIndex: ['archiveData', 'name'],
     },
     {
       key: 'category',
       title: 'Category',
-      dataIndex: 'category',
+      dataIndex: ['archiveData', 'category'],
     },
     {
       key: 'unitPrice',
       title: 'Price',
-      dataIndex: 'unitPrice',
+      dataIndex: ['archiveData', 'unitPrice'],
     },
     {
       key: 'quantityInStock',
       title: 'Quantity',
-      dataIndex: 'quantityInStock',
+      dataIndex: ['archiveData', 'quantityInStock'],
       sorter: (a, b) => a.quantityInStock - b.quantityInStock,
       filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
         <div style={{ padding: 8 }}>
@@ -111,16 +138,28 @@ function index() {
     {
       key: 'orderDate',
       title: 'Order Date',
-      dataIndex: 'orderDate',
+      dataIndex: ['archiveData', 'orderDate'],
       render: (text) => moment(text).format('DD/MM/YY'), // Format the ISO date string
     },
     {
-      key: 'expiryDate',
+      key: `expiryDate`,
       title: 'Expiry Date',
-      dataIndex: 'expiryDate',
+      dataIndex: ['archiveData', 'expiryDate'],
+      render: (text) => moment(text).format('DD/MM/YY'), // Format the ISO date string
+      sorter: (a, b) => moment(a.expiryDate).unix() - moment(b.expiryDate).unix(), // Custom sorting function
+    },
+    {
+      key: 'archiveDate',
+      title: 'Archive Date',
+      dataIndex: 'archiveDate',
       render: (text) => moment(text).format('DD/MM/YY'), // Format the ISO date string
       defaultSortOrder: 'descend',
-      sorter: (a, b) => moment(a.expiryDate).unix() - moment(b.expiryDate).unix(), // Custom sorting function
+      sorter: (a, b) => moment(a.archiveDate).unix() - moment(b.archiveDate).unix(), // Custom sorting function
+    },
+    {
+      key: 'creator',
+      title: 'Creator',
+      dataIndex: 'creator',
     },
   ];
 
@@ -128,58 +167,127 @@ function index() {
     //redirect to login page if unauthenticated
     return <Navigate replace to="/login" />;
   } else {
-    return (
-      <section className="relative">
-        <div className="grid grid-cols-6 grid-rows-1 gap-12">
-          {/* Fixes nav to the left avoid overscroll */}
-          <div className="h-screen sticky top-0">
-            <Navbar />
+    if (loading) {
+      return (
+        <section className="relative">
+          <div className="grid grid-cols-6 grid-rows-1 gap-12">
+            {/* Fixes nav to the left avoid overscroll */}
+            <div className="h-screen sticky top-0">
+              <Navbar />
+            </div>
+            <main className="col-span-5">
+              {/* Positions topbar to be sticky at the top on scroll */}
+              <div className="sticky top-0 left-0 right-0 z-50 bg-white">
+                <Topbar />
+              </div>
+              <div className="h-screen flex flex-col items-center justify-center">
+                <div
+                  className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                  role="status"
+                >
+                  <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                    Loading...
+                  </span>
+                </div>
+                <h1 className="mt-5">Loading archive data...</h1>
+              </div>
+            </main>
           </div>
-          <main className="col-span-5">
-            {/* Positions topbar to be sticky at the top on scroll */}
-            <div className="sticky top-0 left-0 right-0 z-50 bg-white">
-              <Topbar />
+        </section>
+      );
+    }
+
+    if (error) {
+      return (
+        <section className="relative">
+          <div className="grid grid-cols-6 grid-rows-1 gap-12">
+            {/* Fixes nav to the left avoid overscroll */}
+            <div className="h-screen sticky top-0">
+              <Navbar />
             </div>
-            <h1 className="my-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Archived Data
-            </h1>
-            <div ref={componentRef}>
-              <Table dataSource={filteredData} columns={columns} pagination={false} />
+            <main className="col-span-5">
+              {/* Positions topbar to be sticky at the top on scroll */}
+              <div className="sticky top-0 left-0 right-0 z-50 bg-white">
+                <Topbar />
+              </div>
+              <div className="h-screen flex flex-col items-center justify-center">
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-5 h-5 rtl:rotate-180"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
+                    />
+                  </svg>
+                </div>
+                <h1 className="mt-5">Oops! An error ocurred when retrieving archive data...</h1>
+              </div>
+            </main>
+          </div>
+        </section>
+      );
+    }
+    if (archive.length > 0) {
+      return (
+        <section className="relative">
+          <div className="grid grid-cols-6 grid-rows-1 gap-12">
+            {/* Fixes nav to the left avoid overscroll */}
+            <div className="h-screen sticky top-0">
+              <Navbar />
             </div>
-            {/* PRINT DATA DIV */}
-            <div style={{ marginBottom: 16 }}>
-              <RangePicker
-                style={{ marginRight: 8 }}
-                onChange={(dates) => {
-                  console.log(dates);
-                  if (dates && dates.length === 2) {
-                    setStartDate(dates[0].$d.toISOString());
-                    setEndDate(dates[1].$d.toISOString());
-                    console.log(startDate);
-                    console.log(endDate);
-                  }
-                }}
-              />
-              <Button type="primary" onClick={handleDateFilter}>
-                Filter
-              </Button>
-              <Button onClick={clearDateFilter}>Reset</Button>
-            </div>
-            <div>
-              <button
-                onClick={() => {
-                  console.log('printed pdf');
-                  handlePrint();
-                }}
-                className="flex w-48 my-5 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Print report
-              </button>
-            </div>
-          </main>
-        </div>
-      </section>
-    );
+            <main className="col-span-5">
+              {/* Positions topbar to be sticky at the top on scroll */}
+              <div className="sticky top-0 left-0 right-0 z-50 bg-white">
+                <Topbar />
+              </div>
+              <h1 className="my-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                Archived Data
+              </h1>
+              <div ref={componentRef}>
+                <Table dataSource={filteredData} columns={columns} pagination={false} />
+              </div>
+              {/* PRINT DATA DIV */}
+              <div style={{ marginBottom: 16 }}>
+                <RangePicker
+                  style={{ marginRight: 8 }}
+                  onChange={(dates) => {
+                    console.log(dates);
+                    if (dates && dates.length === 2) {
+                      setStartDate(dates[0].$d.toISOString());
+                      setEndDate(dates[1].$d.toISOString());
+                      console.log(startDate);
+                      console.log(endDate);
+                    }
+                  }}
+                />
+                <Button type="primary" onClick={handleDateFilter}>
+                  Filter
+                </Button>
+                <Button onClick={clearDateFilter}>Reset</Button>
+              </div>
+              <div>
+                <button
+                  onClick={() => {
+                    console.log('printed pdf');
+                    handlePrint();
+                  }}
+                  className="flex w-48 my-5 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Print report
+                </button>
+              </div>
+            </main>
+          </div>
+        </section>
+      );
+    }
   }
 }
 
