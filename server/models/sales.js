aa64b454c847ab6d7a66c081dbeea7fc5d5f7f74
alvyynm <alvyynm@gmail.com>
2023-10-19 10:09:27 +0300

    create sale schema

---
+++ server/models/sales.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Define the Sale schema and model
+const saleSchema = new Schema({
+  productId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  saleDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("sale", saleSchema);
