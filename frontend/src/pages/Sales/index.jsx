9e6c5bad5ae419d3837fd7515a1b36ecfb7d305c
alvyynm <alvyynm@gmail.com>
2023-10-20 11:43:52 +0300

    code refactor

--- frontend/src/pages/Sales/index.jsx
+++ frontend/src/pages/Sales/index.jsx
@@ -6,6 +6,8 @@ import moment from 'moment';
 const { RangePicker } = DatePicker;
 import Navbar from '../../components/Navbar';
 import Topbar from '../../components/Topbar';
+import Loading from '../../components/Loading';
+import Error from '../../components/Error';
 import { AuthContext } from '../../context/AuthContext';
 import { SalesContext } from '../../context/SalesContext';
 
@@ -157,38 +159,10 @@ function index() {
             </div>
             {loading ? (
               // loading state
-              <div className="h-screen flex flex-col items-center justify-center">
-                <div
-                  className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-                  role="status"
-                >
-                  <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-                    Loading...
-                  </span>
-                </div>
-                <h1 className="mt-5">Loading sales data...</h1>
-              </div>
+              <Loading />
             ) : error ? (
               // error message if api call fails
-              <div className="h-screen flex flex-col items-center justify-center">
-                <div>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class="w-5 h-5 rtl:rotate-180"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
-                    />
-                  </svg>
-                </div>
-                <h1 className="mt-5">Oops! An error ocurred when retrieving sales data...</h1>
-              </div>
+              <Error />
             ) : (
               // display data after successful retrieval from api
               <div>
